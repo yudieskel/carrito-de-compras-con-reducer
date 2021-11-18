e361605a32e456d94ce0f9e596921492d845a80d
Yudieskel Roque Cardentey (Yuri) <92106853+yudieskel@users.noreply.github.com>
2021-11-17 22:49:05 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import ShoppingCart from "./componets/ShoppingCart";
+
+function App() {
+  return (
+    <div>
+      <h1>useReducer</h1>
+      <hr/>
+      <ShoppingCart/>
+    </div>
+  );
+}
+
+export default App;
